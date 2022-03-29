c1043bb2a293857919a2669d0d85eaee5e4e34d3
Kallil12 <kallilaraujo@gmail.com>
2022-03-29 15:48:03 -0300

    r4ds exercises

---
+++ r4ds/chapter_3.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(Hmisc)
+
+# a graphing template:
+# ggplot(data = <DATA>) +
+# <GEOM_FUNCTION)(mapping = aes(<MAPPINGS))
+
+# 3.2.4 exercises
+
+print("3.2.4 - Exercises")
+print("1")
+
+ggplot(data = mpg)
+# it only shows a blank graph because there is not mapping. No X
+# or Y associated with the variables
+
+print("2")
+mpg
+print("a tibble 234x11, therefore 234 lines and 11 columns")
+
+print("3")
