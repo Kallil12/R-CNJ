d74a92213301621b8d6c2e70745387ab5f31253e
Kallil12 <kallilaraujo@gmail.com>
2022-03-30 17:56:49 -0300

    adicionando questões do cap 3 do r4ds

--- r4ds/chapter_3.R
+++ r4ds/chapter_3.R
@@ -19,3 +19,17 @@ mpg
 print("a tibble 234x11, therefore 234 lines and 11 columns")
 
 print("3")
+?mpg
+print("drv describes the type of drivetrain. If it is front, rear or all wheel drive.")
+
+print("4")
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = cyl, y = hwy))
+
+print("5")
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = drv, y = class))
+
+print("both axis are formed by categorical data, so it is not very useful to compare them because they overlap each other")
