365e16200d8b3232a3561f93f389c90e78bf2758
Kallil12 <kallilaraujo@gmail.com>
2022-08-15 16:06:41 -0300

    r4ds chap 13

--- r4ds/r4ds/r4ds_chap_13.R
+++ r4ds/r4ds/r4ds_chap_13.R
@@ -123,4 +123,110 @@ flights2 %>%
 
 # exercises ----
 
+airports %>%
+  semi_join(flights, c("faa" = "dest")) %>%
+  ggplot(aes(lon, lat)) +
+    borders("state") +
+    geom_point() +
+    coord_quickmap()
+
+avg_dest_delays <- flights %>%
+  group_by(dest) %>%
+  summarise(delay = mean(arr_delay, na.rm = TRUE)) %>%
+  inner_join(airports, by = c(dest = "faa")) 
+
+avg_dest_delays %>%
+  ggplot(aes(lon, lat, color = delay)) +
+  borders("state") + 
+  geom_point() +
+  coord_quickmap()
+
+# 2 add the location of the origin and destination to flights
+
+airport_locations <- airports %>%
+  select(faa, lat, lon)
+
+flights %>%
+  select(year:day, hour, origin, dest) %>%
+  left_join(
+    airport_locations,
+    by = c("origin" = "faa")
+  ) %>%
+  left_join(
+    airport_locations,
+    by = c("dest" = "faa")
+  )
+
+airport_locations <- airports %>%
+  select(faa, lat, lon)
+
+flights %>%
+  select(year:day, hour, origin, dest) %>%
+  left_join(
+    airport_locations, 
+    by = c("origin" = "faa")
+  ) %>%
+  left_join(
+    airport_locations,
+    by = c("dest" = "faa"),
+    suffix = c("_origin", "_dest")
+  )
+
+# is there a relationship between the age of a plane and its delays?
+
+plane_cohorts <- inner_join(flights,
+                            select(planes, tailnum, plane_year = year),
+                            by = "tailnum"
+                ) %>%
+  mutate(age = year - plane_year) %>%
+  filter(!is.na(age)) %>%
+  mutate(age = if_else(age > 25, 25L, age)) %>%
+  group_by(age) %>%
+  summarise(
+    dep_delay_mean = mean(dep_delay, na.rm = TRUE),
+    dep_delay_sd = sd(dep_delay, na.rm = TRUE),
+    arr_delay_mean = mean(arr_delay, na.rm = TRUE),
+    arr_delay_sd = sd(arr_delay, na.rm = TRUE),
+    n_arr_delay = sum(!is.na(arr_delay)),
+    n_dep_delay = sum(!is.na(dep_delay))
+  )
+
+ggplot(plane_cohorts, aes(x = age, y = dep_delay_mean)) +
+  geom_point() +
+  scale_x_continuous("Planes' age (years)", breaks = seq(0,30, by = 10)) +
+  scale_y_continuous("Mean departure delay (minutes)")
+
+ggplot(plane_cohorts, aes(x = age, y = arr_delay_mean)) +
+  geom_point() +
+  scale_x_continuous("Planes' age (years)", breaks = seq(0,30, by = 10)) +
+  scale_y_continuous("Mean departure delay (minutes)")
+
+# what weather conditions make it more likely to see a delay?
+
+flight_weather <- flights %>%
+  inner_join(weather, by = c("origin" = "origin", 
+                             "year" = "year", 
+                             "month" = "month", 
+                             "day" = "day", 
+                             "hour" = "hour"))
+
+flight_weather %>%
+  group_by(precip) %>%
+  summarise(delay = mean(dep_delay, na.rm = TRUE)) %>%
+  ggplot(aes(x = precip, y = delay)) +
+  geom_line() + geom_point()
+
+
+flights %>%
+  filter(year == 2013, month == 6, day == 13) %>%
+  group_by(dest) %>%
+  summarise(delay = mean(arr_delay, na.rm = TRUE)) %>%
+  inner_join(airports, by = c("dest" = "faa")) %>%
+  ggplot(aes(y = lat, x = lon, size = delay, colour = delay)) +
+  borders("state") +
+  geom_point() +
+  coord_quickmap()
+
+# filtering joins
+
 
